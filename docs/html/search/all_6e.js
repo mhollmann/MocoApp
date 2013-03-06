d5209da7ea9e9f8ab3ccde3bb2908ab7927a6a8f
mhollmann <maurice.hollmann@yahoo.de>
2013-03-06 16:41:15 +0100

    Update API documentation

--- docs/html/search/all_6e.js
+++ docs/html/search/all_6e.js
@@ -1,5 +1,6 @@
 var searchData=
 [
+  ['neweddataelementfromfile_3a',['newEDDataElementFromFile:',['../df/de1/interface_moco_registration.html#a677416a052d27c48157c11d694917c1e',1,'MocoRegistration']]],
   ['numberofiterations',['NumberOfIterations',['../d3/d3c/interface_moco_registration_property.html#a9e2b22aa25f78f18b6e7ca104ed2e869',1,'MocoRegistrationProperty']]],
   ['numberofthreads',['NumberOfThreads',['../d3/d3c/interface_moco_registration_property.html#adadf36ddf6f75991e53468b70fcc602c',1,'MocoRegistrationProperty']]]
 ];
--- docs/html/search/classes_63.js
+++ docs/html/search/classes_63.js
@@ -1,4 +1,5 @@
 var searchData=
 [
+  ['castpixelaccessor',['CastPixelAccessor',['../d0/da6/class_cast_pixel_accessor.html',1,'']]],
   ['commanditerationupdate',['CommandIterationUpdate',['../d0/daa/class_command_iteration_update.html',1,'']]]
 ];
